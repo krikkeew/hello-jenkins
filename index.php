c5a60066c5c9539a6ced8eb5d710a693e34610e2
Leon Tanjaya <leontanjaya@gmail.com>
2014-08-30 16:46:15 +0700

    fourth commit

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
  	<?php echo '<p>Hello World</p>'; ?> 
  	<?php echo '<p>Hope you are doing well!</p>'; ?> 
  	<?php echo '<p>See you!</p>'; ?> 
+ 	<?php echo '<p>See you there!</p>'; ?> 
  </body>
 </html>
